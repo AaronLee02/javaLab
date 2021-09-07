aed331bae593a8cd66dcb5f8bb2fbad084d7ed10
AaronLee02 <soongyu222@naver.com>
2021-09-07 17:39:02 +0900

    .

---
+++ chap06Lab/src/sec04/exam01_class_new/StudentExample.java
@@ -0,0 +1,27 @@
+package sec04.exam01_class_new;
+
+// 실행용 클래스
+public class StudentExample {
+
+	public static void main(String[] args) {
+
+		Student s1 = new Student(); // Student 객체를 만들어 생성된 객체의 주소값을 s1 변수에 저장한다.
+									// s1은 Student 객체를 참조하므로 s1은 생성된 Student 객체를 지칭한다.
+		System.out.println("s1변수가 Student 객체를 참조합니다.");
+		
+		Student s2 = new Student();
+		System.out.println("s1변수가 또 다른 Student 객체를 참조합니다.");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//main
+
+}//class
